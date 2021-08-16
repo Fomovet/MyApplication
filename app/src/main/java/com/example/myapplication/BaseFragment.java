4a45dba2c4fe8494d26f1b7fbc9901667a5bb7b2
Fomovet@gmail.com <android123>
2021-08-16 20:33:30 +0800

    修复

--- app/src/main/java/com/example/myapplication/BaseFragment.java
+++ app/src/main/java/com/example/myapplication/BaseFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,12 +17,12 @@ import org.jetbrains.annotations.NotNull;
 public abstract class BaseFragment<T extends ViewBinding> extends AwesomeFragment {
     protected T mBinding;
 
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,  ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-
         mBinding = ViewBindingUtil.create(getClass(),inflater.from(getActivity()),container,false);
-        return mBinding.getRoot();
+        FrameLayout frameLayout = new FrameLayout(getActivity());
+        frameLayout.addView(mBinding.getRoot());
+        return frameLayout;
     }
 
     @Override
@@ -30,9 +31,6 @@ public abstract class BaseFragment<T extends ViewBinding> extends AwesomeFragmen
         initEventAndData();
     }
 
-
-
     protected abstract void initEventAndData();
-
 
 }
--- app/src/main/java/com/example/myapplication/TestFragment.java
+++ app/src/main/java/com/example/myapplication/TestFragment.java
@@ -17,15 +17,10 @@ import com.navigation.androidx.AwesomeFragment;
  * Email : Fomovet@Gmail.com
  * 创建时间 : 2021/08/14 18:42
  */
-public class TestFragment extends AwesomeFragment {
+public class TestFragment extends BaseFragment<FramgentTestOneBinding> {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        //使用ViewBinding报错写法一：FragmentTestBinding.inflate(inflater,container,false).getRoot()
-        //报错写法二：FragmentTestBinding.inflate(getLayoutInflater()).getRoot()
-        //不报错写法：inflater.inflate(R.layout.fragment_test, container, true)
-        return FramgentTestOneBinding.inflate(getLayoutInflater()).getRoot();
+    protected void initEventAndData() {
+        mBinding.mText.setText("");
     }
 }
